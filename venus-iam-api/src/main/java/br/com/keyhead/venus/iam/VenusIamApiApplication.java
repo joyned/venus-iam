ba53eb04a97ceda6410148c830947d07c5a83fb3
joyned <brunodsbg@gmail.com>
2021-11-23 20:01:44 -0300

    First Commit

---
+++ venus-iam-api/src/main/java/br/com/keyhead/venus/iam/VenusIamApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.keyhead.venus.iam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenusIamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenusIamApiApplication.class, args);
+	}
+
+}
---
+++ venus-iam-api/src/test/java/br/com/keyhead/venus/iam/VenusIamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.keyhead.venus.iam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenusIamApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
